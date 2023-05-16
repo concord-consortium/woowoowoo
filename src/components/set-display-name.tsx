b04a59a5af07898ef2f6cca3974dca5cd5590e7a
Doug Martin <dmartin@concord.org>
2023-05-16 15:15:38 -0400

    Added start of visualization

--- src/components/set-display-name.tsx
+++ src/components/set-display-name.tsx
@@ -25,7 +25,7 @@ export const SetDisplayName = ({state: {displayName}, setState}: ComponentProps)
         <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
           <div className="px-5 py-7">
             <label className="font-semibold text-sm text-gray-600 pb-1 block">Display Name</label>
-            <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" value={displayName} ref={displayNameRef} />
+            <input type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" defaultValue={displayName||""} ref={displayNameRef} />
             <button type="submit" className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
                 <span className="inline-block mr-2">Continue</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block">
